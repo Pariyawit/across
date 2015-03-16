dd8749c731b8ae202cc01b3c61573f84300d4e3e
Pariyawit <pariyawit.jat@gmail.com>
2015-03-16 22:41:39 +0700

    faster bold keyword

--- app/controllers/SummaryController.php
+++ app/controllers/SummaryController.php
@@ -13,24 +13,16 @@ class SummaryController extends \BaseController {
 		$scores = Average::getAverage($title);
 
 		$summary = SummaryController::boldKeyword($summary);
+		// return $summary;
 		return View::make('hotel',['info'=>$info, 'summary'=>$summary ,'scores'=>$scores]);
 	}
 
 	public static function boldKeyword($summary){
 
 		// $result = shell_exec('python public/script/boldkeyword.py '.escapeshellarg(json_encode($summary)) );
-		$output = [];
-		foreach ($summary as $topic) {
-			$key = array_keys($summary,$topic);
-			$key = $key[0];
-			$output[$key] = array();
-			foreach ($topic as $sentence) {
-				$result = shell_exec('python public/script/boldkeyword.py '.$key.' '.escapeshellarg(json_encode($sentence)) );
-				// var_dump($result);
-				array_push($output[$key],$result);
-			}
-		}
-		var_dump($output);
-		return $output;
+
+		$result = shell_exec('python public/script/boldkeyword.py '.(json_encode(json_encode($summary))) );
+
+		return json_decode($result,true);
 	}
 }
--- public/script/boldkeyword.py
+++ public/script/boldkeyword.py
@@ -2,7 +2,7 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk import WordPunctTokenizer
 import sys,json,os
 
-# en_stem = SnowballStemmer("english")
+en_stem = SnowballStemmer("english")
 wpt = WordPunctTokenizer()
 # stopwords_list = []
 # stopwords_list.extend(stopwords.words('english')) 
@@ -10,20 +10,29 @@ wpt = WordPunctTokenizer()
 
 # topic = ['cleanliness','comfort','location','staff','value']
 
-topic = sys.argv[1];
-sentence = sys.argv[2];
+#except_word = ['so far','besides']
+
+# topic = sys.argv[1];
+
+data = json.loads(sys.argv[1]);
+
 
 
 script_dir = os.path.dirname(__file__)
-json_data=open(script_dir+'/keywords.json')
-# json_data=open('keywords.json')
+json_data=open(script_dir+'/stem_keywords.json')
+# json_data=open('stem_keywords.json')
 keywords = json.load(json_data)
 json_data.close()
 
-tokens = wpt.tokenize(sentence)
-for token in tokens:
-	if(token in keywords[topic]):
-		# print token
-		sentence = sentence.replace(token,"<strong>"+token+"</strong>");
-# stem = en_stem.stem(token)
-print sentence
+output = dict();
+for topic in data:
+	output[topic] = list()
+	for sentence in data[topic]:
+		tokens = wpt.tokenize(sentence)
+		for token in tokens:
+			if(en_stem.stem(token) in keywords[topic]):
+				sentence = sentence.replace(token,"<strong>"+token+"</strong>");
+		output[topic].append(sentence)
+
+print json.dumps(output);
+
