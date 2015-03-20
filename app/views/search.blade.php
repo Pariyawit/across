52c46ee235db65b145549b8d90f97d48ad8991e1
Pariyawit <pariyawit.jat@gmail.com>
2015-03-21 00:30:08 +0700

    add search description

--- app/views/search.blade.php
+++ app/views/search.blade.php
@@ -15,41 +15,27 @@
 		 	</div>
 		{{ Form::close() }}
 	  </div>
-	  		<div class="col-sm-12">
+	  	<div class="col-sm-12">
 			<h3>Search result for : "{{ $search }}"</h3>
 				{{-- <h3>&nbsp;&nbsp;&nbsp;"{{ $search }}"</h3> --}}
+		</div>
+		<div class="col-sm-6">
+			seach by keywords matched, title, address, city
 		</div>
 	</div>
 </div>
 
 <div class="jumbotron search wow fadeIn">
-	{{-- <div class="row">
-		<div class="col-sm-8 col-sm-offset-2">
-		{{ Form::open(['url' => 'search','method' => 'get']) }}
-			<div class="input-group">
-			 		{{Form::text('search',$search,["class"=>"form-control input-lg"])}}
-			 		<span class="input-group-btn">
-				 		{{Form ::button('<span class="glyphicon glyphicon-search">',
-				 				["type" => "submit","class" => "btn btn-default btn-lg"])}}
-			 		</span>
-		 	</div>
-		{{ Form::close() }}
-	  </div>
-	</div> --}}
 	<div class="row">
-		<div class="col-sm-4">
-			{{-- <h3>Search result for :</h3>
-				<h3>&nbsp;&nbsp;&nbsp;"{{ $search }}"</h3> --}}
-		</div>
 		<table class="table table-hover result">
 		<thead>
 			<tr>
 				<th class="hotelname">HOTEL</th>
 				<th class="city">CITY</th>
 				{{-- <th>Booking</th> --}}
-				<th><img src="/assets/pic/booking.jpg"></th>
-				<th><img src="/assets/pic/agoda.png"></th>
-				<th><img src="/assets/pic/tripadvisor.jpg"></th>
+				<th class="site"><img src="/assets/pic/booking.jpg"></th>
+				<th class="site"><img src="/assets/pic/agoda.png"></th>
+				<th class="site"><img src="/assets/pic/tripadvisor.jpg"></th>
 				{{-- <th>Agoda</th> --}}
 				{{-- <th>TripAdvisor</th> --}}
 				<th id="logo">ACROSS</th>
